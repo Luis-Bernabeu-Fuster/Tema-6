aa1dd61a28990354e8a74539c7735ab4e3f85833
Luis-Bernabeu-Fuster <luiberfus@alu.edu.gva.es>
2025-01-22 11:31:23 +0100

    Delete Actividad1/Ejercicio12/Actividad13 directory

--- Actividad1/Ejercicio12/Actividad13/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Circulo circulo = new Circulo(12.3);
-        Cilindro cilindro = new Cilindro(5.67,10.0);
-
-        System.out.println(circulo);
-        System.out.println(cilindro);
-    }
-}
