ae9a81f42a1dec7113f9ab4ec4c505e962a5ee07
Luis-Bernabeu-Fuster <luiberfus@alu.edu.gva.es>
2025-01-22 11:29:48 +0100

    Add files via upload

---
+++ Actividad1/Ejercicio12/Actividad13/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo(12.3);
+        Cilindro cilindro = new Cilindro(5.67,10.0);
+
+        System.out.println(circulo);
+        System.out.println(cilindro);
+    }
+}
